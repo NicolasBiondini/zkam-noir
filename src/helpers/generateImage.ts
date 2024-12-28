9b74fa04276db14ec65863fb1ee0971f42469033
Yago Pajariño <yagopajarino@outlook.com>
2024-12-28 18:39:51 -0300

    Make processImage async

--- src/helpers/generateImage.ts
+++ src/helpers/generateImage.ts
@@ -1,10 +1,10 @@
 import { Hasher } from "./Hasher";
 import { ImageHashProcessor } from "./ImageHashProcessor";
 
-export function processImage(
+export async function processImage(
   imageData: Uint8ClampedArray,
   indices: number[]
-): Uint8ClampedArray {
+): Promise<Uint8ClampedArray> {
   const imageProcessor = new ImageHashProcessor(imageData, indices);
   const pixelToHash = imageProcessor.getPixelsToHash();
   const hash = await new Hasher(pixelToHash).hashData();
